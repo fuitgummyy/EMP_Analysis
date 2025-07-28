0ce186c060b37868a9d422aeff0b992c22e2c061
Atlas H. <valentinahussey@gmail.com>
2025-07-28 09:37:23 -0600

    debugging, added mag field calculations
    changed signal to include DC term, 
    enforced hermitian symmetry, (where the negatve freqs are complex conjugates of the pos freqs)
    
    performed and included calculations for left and right circular of mag field

--- fft_ionosphere.py
+++ fft_ionosphere.py
@@ -30,43 +30,74 @@ n = signal_f.size
 time_interval = 0.001 #time in microseconds
 signal_f_freq = np.fft.fftfreq(n, d=time_interval)
 STEC = 10 #Slant TEC (total electron content) 
+   
 
-    
-#removes DC term  of dataset   
-signal_f = signal_f[1:]
-signal_f_freq = signal_f_freq[1:] 
-
-signal_f[:2500] = 0
-signal_f[-2500:] = 0
+#highpass
+signal_f[:7500] = 0
+signal_f[-7500:] = 0
 signal_f_mag= np.abs(signal_f)
-#minimum freq for highpass
 
+
+no_mag_phase = []
+right_circular_phase = []
+left_circular_phase = []
+for freq in signal_f_freq:
+    if freq == 0:
+        no_mag_phase.append(freq)
+        right_circular_phase.append(freq)
+        left_circular_phase.append(freq)
+        
+    else:
+        no_mag_phase.append(-8445*STEC/(2*cmath.pi * freq))
+        right_circular_phase.append((-8445*STEC)/((freq) *(1 - 0.5*(17.5 * 0.5))/ freq))
+        left_circular_phase.append((-8445*STEC)/((freq) *(1 +  0.5*(17.5 * 0.5))/ freq))
+
+no_mag_phase = np.array(no_mag_phase)
+right_circular_phase = np.array(right_circular_phase)
+left_circular_phase = np.array(left_circular_phase)
+
+#enforces Hermitian symmetry
+def enforce_symmetry (input_signal):
+    N = np.size(input_signal)
+    for i in range (1,(N +1) //2):
+        input_signal[N-i] = np.conjugate(input_signal[i])
+    return input_signal
 
 #calculations for phase shift caused by ionosphere
-change_in_phase = -8445*STEC/(2*cmath.pi * signal_f_freq)
-ionosphere_phase = np.exp(-1j * change_in_phase)
-out_of_ionosphere = signal_f * (ionosphere_phase)
-signal_t_satellite = np.fft.ifft(out_of_ionosphere)
-t_satellite_mag = np.abs(signal_t_satellite)
-iono_mag =  np.abs(out_of_ionosphere)
+def phaseshift(phase):
+    ionosphere_phase = np.exp(-1j * phase)
+    out_of_ionosphere = signal_f * (ionosphere_phase)
+    #signal_t_satellite = np.fft.ifft(iono_symmetry)
+    #t_satellite_mag = np.abs(signal_t_satellite)
+    iono_mag =  np.abs(out_of_ionosphere)
+    return out_of_ionosphere
 
-#magnetic field
-right_circular = 8445*STEC/(2*cmath.pi * signal_t_satellite) *(1 - 0.5*(17.5 * 0.5)/signal_t_satellite)
-left_circular = 8445*STEC/(2*cmath.pi * signal_f_freq) *(1 +  0.5*(17.5 * 0.5)/signal_f_freq)
-    
-    #for debug purposes:
-#debug_df = pd.DataFrame( data = {'signal_t': (signal_t)
-                                 #'change_in_phase': (change_in_phase)})
-                                     #'out_of_ionosphere': (out_of_ionosphere),
+
+no_mag = np.fft.ifft(enforce_symmetry(phaseshift(no_mag_phase)))
+right_circular = np.fft.ifft(enforce_symmetry(phaseshift(right_circular_phase)))
+left_circular = np.fft.ifft(enforce_symmetry(phaseshift(left_circular_phase)))
+both = right_circular + left_circular
+
+
+
+#for debug purposes:
+#debug_df = pd.DataFrame( data = {'no': (no_mag),
+                                 #'right': (right_circular_phase),
+                                 #'left': (left_circular_phase),
                                      #'t_satellite: (signal_t_satellite)
                                      #})
 
     
-df = pd.DataFrame(data = {'real': (signal_t_satellite.real),
-                          'imag':(signal_t_satellite.imag),
-                         'mag': (t_satellite_mag)
-                          #'complex':(signal_t_satellite.real + signal_t_satellite.imag*0j)
+df = pd.DataFrame(data = {'no_mag_real':(no_mag.real),
+                          'no_mag_imag':(no_mag.imag),
+                          'right_real': (right_circular.real),
+                          'right_imag':(right_circular.imag),
+                          'left_real':(left_circular.real),
+                          'left_imag':(left_circular.imag),
+                          'both_real':(both.real),
+                          'both_imag':(both.imag)
                           })
 
-df.to_csv(path_or_buf="fft_case_01_satellite.dat", sep=" ", index = True, header=[ 'real', 'imag', 'mag' ], float_format='%.6e')
+df.to_csv(path_or_buf="fft_case_01_satellite.dat", sep=" ", index = True, header=[ 'no_mag_real','no_mag_imag',  'right_real', 'right_imag', 'left_real','left_imag', 'both_real','both_imag' ], float_format='%.6e')
+
 
